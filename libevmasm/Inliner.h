bede14caff8ed9e0f268594b86d85a0323066082
Daniel Kirchner <daniel@ekpyron.org>
2021-01-14 17:21:39 +0100

    Some test cases.

--- libevmasm/Inliner.h
+++ libevmasm/Inliner.h
@@ -34,7 +34,7 @@ using AssemblyItems = std::vector<AssemblyItem>;
 class Inliner
 {
 public:
-	explicit Inliner(AssemblyItems& _items, int _inlineMaxOpcodes = 5): m_items(_items), m_inlineMaxOpcodes(_inlineMaxOpcodes) {}
+	explicit Inliner(AssemblyItems& _items, int _inlineMaxOpcodes = 6): m_items(_items), m_inlineMaxOpcodes(_inlineMaxOpcodes) {}
 	virtual ~Inliner() = default;
 
 	bool isInlineCandidate(ranges::span<AssemblyItem const> _items) const;
